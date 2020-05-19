b531f7e02979b1ae72e894cc9f00c1892b081b93
joseph <joseph.siyi@gmail.com>
2020-05-20 06:16:18 +0800

    first commit

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Configuration;
+
+namespace SendGridAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotificationController : Controller
+    {
+       private readonly IConfiguration _configuration;
+
+       public NotificationController(IConfiguration configuration)
+       {
+         _configuration = configuration;
+       }      
+    
+       [Route("SendNotification")]
+       public async Task PostMessage()
+       {
+          var apiKey = _configuration.GetSection("SENDGRID_API_KEY").Value;
+          var client = new SendGridClient(apiKey);
+          var from = new EmailAddress("test1@example.com", "Example User 1");
+          List<EmailAddress> tos = new List<EmailAddress>
+          {
+              new EmailAddress("joseph.siyi@gmail.com", "Example User 2"),
+              new EmailAddress("chenkiegcp1@gmail.com", "Example User 3"),
+              new EmailAddress("chenkiegcp1@gmail.com","Example User 4")
+          };
+        
+          var subject = "Hello world email from Sendgrid ";
+          var htmlContent = "<strong>Hello world with HTML content</strong>";
+          var displayRecipients = false; // set this to true if you want recipients to see each others mail id 
+          var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, tos, subject, "", htmlContent, false);
+          var response = await client.SendEmailAsync(msg);
+      }
+   }
+}
